c6101d58121835d828b2369eb8402ad3d9546314
Bruce Jagid <brucejagid@gmail.com>
2023-12-25 00:34:42 -0500

    added main.c since I like its current state as a framework for testing http lib

--- main.c
+++ main.c
@@ -1,8 +1,66 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "http/cookie.h"
+#include "http/client.h"
+#include "http/response.h"
+#include "http/request.h"
+#include "lib/dictcase.h"
+#include "lib/dict.h"
 
 int 
-main(void)
+main(int argc, char *argv[])
 {
-    printf("Hello world!");
+    struct bksmt_http_req *req;
+    struct bksmt_http_res *res;
+    struct bksmt_http_client *client;
+    struct bksmt_http_status_lut_entry le;
+    struct bksmt_dict_elem *de;
+    struct bksmt_dictcase_elem *dce;
+    char *val;
+    int stat, i;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: ./booksmart uri\n");
+        return -1;
+    }
+
+    stat = bksmt_http_req_init(argv[1], HTTP_GET, NULL, 0, &req);
+    if (stat == HTTP_ERROR){
+        fprintf(stderr, "error: improper uri\n");
+        return -1;
+    }
+
+    client = bksmt_http_client_init();
+
+    stat = bksmt_http_client_do(client, req, 0, &res);
+    if (stat == HTTP_ERROR){
+        fprintf(stderr, "error: invalid message or network error\n");
+        return -1;
+    }
+
+    le = bksmt_http_status_lut[res->header.statk];
+    printf("HTTP/%d.%d %s %s\n", res->header.vmajor, 
+            res->header.vminor, le.code, le.stat_msg);
+
+    if (res->header.mfields)
+        BKSMT_DICT_FOREACH(res->header.mfields, de)
+            printf("%s: %s\n", de->key, de->val);
+
+    if (res->header.cookies)
+        BKSMT_DICTCASE_FOREACH(res->header.cookies, dce) {
+            val = bksmt_dict_get(dce->val, "Value");
+            printf("Set-Cookie: %s=%s", dce->key, val);
+            BKSMT_DICT_FOREACH(dce->val, de) {
+                if (strcmp(de->key, "Name") && strcmp(de->key, "Value"))
+                    printf("; %s=%s", de->key, de->val);
+            }
+            printf("\n");
+        }
+
+    if (res->body)
+        printf("%s.*\n", res->body->end - res->body->start,
+                res->body->inf.mbuf + res->body->start);
+
     return 0;
 }
