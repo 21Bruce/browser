f75b4361fcd508110b9df1571a0874922b912691
Bruce Jagid <brucejagid@gmail.com>
2024-01-07 11:04:18 -0500

    implemented other aes ciphers

--- crypto/aes.c
+++ crypto/aes.c
@@ -6,8 +6,6 @@
 #include "aes_op.h"
 #include <stdio.h>
 
-
-
 void 
 bksmt_aes_128(unsigned char in[16], unsigned char key[16], unsigned char out[16])
 {
@@ -37,6 +35,74 @@ bksmt_aes_128(unsigned char in[16], unsigned char key[16], unsigned char out[16]
 
     /* add last roundkey word */
     bksmt_aes_addroundkey(state, sched + 160);
+
+    /* build output stream from state table */
+    bksmt_aes_out(state, out);
+}
+
+void 
+bksmt_aes_192(unsigned char in[16], unsigned char key[24], unsigned char out[16])
+{
+    unsigned char state[16], sched[208];
+    int round, i, j;
+
+    /* parse input stream into state table */
+    bksmt_aes_in(in, state);
+
+    /* expand key */
+    bksmt_aes_kexp(key, sched, 6);
+
+    /* add first round key */
+    bksmt_aes_addroundkey(state, sched);
+
+    /* do rounds */
+    for(round = 1; round < 12; round++) {
+        bksmt_aes_subbytes(state);
+        bksmt_aes_shiftrows(state);
+        bksmt_aes_mixcols(state);
+        bksmt_aes_addroundkey(state, sched + round * 16);
+    }
+
+    /* final round requires special handling */
+    bksmt_aes_subbytes(state);
+    bksmt_aes_shiftrows(state);
+
+    /* add last roundkey word */
+    bksmt_aes_addroundkey(state, sched + 192);
+
+    /* build output stream from state table */
+    bksmt_aes_out(state, out);
+}
+
+void 
+bksmt_aes_256(unsigned char in[16], unsigned char key[32], unsigned char out[16])
+{
+    unsigned char state[16], sched[240];
+    int round, i, j;
+
+    /* parse input stream into state table */
+    bksmt_aes_in(in, state);
+
+    /* expand key */
+    bksmt_aes_kexp(key, sched, 8);
+
+    /* add first round key */
+    bksmt_aes_addroundkey(state, sched);
+
+    /* do rounds */
+    for(round = 1; round < 14; round++) {
+        bksmt_aes_subbytes(state);
+        bksmt_aes_shiftrows(state);
+        bksmt_aes_mixcols(state);
+        bksmt_aes_addroundkey(state, sched + round * 16);
+    }
+
+    /* final round requires special handling */
+    bksmt_aes_subbytes(state);
+    bksmt_aes_shiftrows(state);
+
+    /* add last roundkey word */
+    bksmt_aes_addroundkey(state, sched + 224);
 
     /* build output stream from state table */
     bksmt_aes_out(state, out);
