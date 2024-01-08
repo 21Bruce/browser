a15e1acfd1c41e82076ac2927cda353dca3cc0f4
Bruce Jagid <brucejagid@gmail.com>
2024-01-08 11:05:20 -0500

    added le pack functions in prep for chacha20 and more documentation for aes constants

--- lib/pack.c
+++ lib/pack.c
@@ -49,3 +49,49 @@ bksmt_unpackbe64(uint64_t src, unsigned char out[8])
     }
 }
 
+uint32_t
+bksmt_packle32(unsigned char src[4])
+{
+    int i; 
+    uint32_t ret;
+
+    ret = 0;
+    for(i = 0; i < 4; i++)
+        ret |= src[i] << (8 * i);
+
+    return ret;
+}
+
+void 
+bksmt_unpackle32(uint32_t src, unsigned char out[4])
+{
+    int i;
+    for (i = 0; i < 4; i++) {
+        out[i] = src >> (8 * i);
+    }
+}
+
+uint64_t
+bksmt_packle64(unsigned char src[8])
+{
+    int i; 
+    uint64_t ret, tmp;
+
+    ret = 0;
+    for(i = 0; i < 8; i++) {
+        tmp = src[i];
+        ret |= tmp << (8 * i);
+    }
+
+    return ret;
+}
+
+void 
+bksmt_unpackle64(uint64_t src, unsigned char out[8])
+{
+    int i;
+    for (i = 0; i < 8; i++) {
+        out[i] = src >> (8 * i);
+    }
+}
+
--- lib/pack.h
+++ lib/pack.h
@@ -27,7 +27,7 @@ void bksmt_unpackbe32(uint32_t, unsigned char[4]);
  */
 uint64_t bksmt_packbe64(unsigned char[8]);
 
-/* put a big endian uint32 in a 4 byte stream 
+/* put a big endian uint64 in a 4 byte stream 
  * Ex:  
  * Input:  0x6162636465666768 
  *         00 01 02 03 04 05 06 07
@@ -35,5 +35,36 @@ uint64_t bksmt_packbe64(unsigned char[8]);
  */
 void bksmt_unpackbe64(uint64_t, unsigned char[8]);
 
+/* make a little endian uint32 from a 4 byte stream.
+ * Ex:  
+ *         00 01 02 03
+ * Input:  61 62 63 64 
+ * Output: 0x64636261
+ */
+uint32_t bksmt_packle32(unsigned char[4]);
+
+/* put a little endian uint32 in a 4 byte stream 
+ * Ex:  
+ * Input:  0x61626364 
+ *         00 01 02 03
+ * Output: 64 63 62 61 
+ */
+void bksmt_unpackle32(uint32_t, unsigned char[4]);
+
+/* make a little endian uint64 from an 8 byte stream.
+ * Ex:  
+ *         00 01 02 03 04 05 06 07
+ * Input:  61 62 63 64 65 66 67 68
+ * Output: 0x6867666564636261 
+ */
+uint64_t bksmt_packle64(unsigned char[8]);
+
+/* put a little endian uint32 in a 4 byte stream 
+ * Ex:  
+ * Input:  0x6162636465666768 
+ *         00 01 02 03 04 05 06 07
+ * Output: 68 67 66 65 64 63 62 61
+ */
+void bksmt_unpackle64(uint64_t, unsigned char[8]);
 
 #endif /* __BKSMT_LIB_PACK_H__ */
